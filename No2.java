82a4d127a8822154cf0352537c66d0b116b5ceab
Hanif Khoirul Fathansyah <37774179+hanifkhoirulf-6706161055@users.noreply.github.com>
2018-03-28 12:02:17 +0700

    Update No2.java

--- No2.java
+++ No2.java
@@ -16,12 +16,12 @@ public class No2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-           int l = 1;
+               int l = 1;
     int tingkat = 10;
     int n = 12/2;
     for(int i=1;i<=n;i++){
     for(int k=n;k>i;k--){
-    System.out.print(" ");
+    System.out.print("0");
     }
     for(int j=1;j<=l;j++){
     System.out.print("+");
@@ -34,7 +34,7 @@ public class No2 {
 
     for(int i=(n-1);i>=1;i--){
     for(int k=i;k<=(n-1);k++){
-    System.out.print(" ");
+    System.out.print("0");
     }
     for(int j=l;j>=1;j--){
     System.out.print("+");
@@ -42,6 +42,7 @@ public class No2 {
     l-=2;
 
     System.out.println("");
+    }
     }
     }
     
